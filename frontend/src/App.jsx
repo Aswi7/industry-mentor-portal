f19e31292b41d4d325c64f67d7727b4cbe92c6a9
aswi7 <aswithaa17@gmail.com>
2026-02-20 11:08:46 -0800

    created student dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import MentorDashboard from "./pages/MentorDashboard"
+import StudentDashboard from "./pages/StudentDashboard"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/register" element={<Register />} />
     
       <Route path="/mentor" element={<MentorDashboard />} />
+       <Route path="/student" element={<StudentDashboard />} />
     
     </Routes>
   )
